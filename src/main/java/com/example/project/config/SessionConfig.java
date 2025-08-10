20b4471e3b5dfaa3f840fff4f5957d38e6e853ed
syncline <syncline139@gmail.com>
2025-08-11 01:44:30 +1000

    Delete SessionConfig

--- src/main/java/com/example/project/config/SessionConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600 * 24) // 24 часа
-public class SessionConfig {}
